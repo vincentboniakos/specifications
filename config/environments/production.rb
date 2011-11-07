6d7912207b82b7260438ef7c15dde7f3594737b2
Vincent Boniakos <vincent.boniakos@gmail.com>
2011-11-07 22:39:25 +0100

    config action mailer for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,4 +59,15 @@ Specifications::Application.configure do
   config.active_support.deprecation = :notify
 
   config.assets.compile = true
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  config.action_mailer.delivery_method = :smtp
+
 end
