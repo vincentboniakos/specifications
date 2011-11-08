d4d21692973cfbb491ace576dec2fe0dff66b22d
Vincent Boniakos <vincent.boniakos@gmail.com>
2011-11-08 19:37:27 +0100

    Put Action Mailer config for mailgun in a initializer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "5emegauche.com",
-  :user_name            => "vincent.b",
-  :password             => "1transform!",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-#Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'] 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'specifications-preprod.heroku.com'
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
