b104417e3913c76316e9f509f1ebc3626f4e5ace
Vincent Boniakos <vincent.boniakos@gmail.com>
2011-11-08 19:30:23 +0100

    add mailgun config setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,14 @@ Specifications::Application.configure do
 
   config.assets.compile = true
 
-  
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'] 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com'
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
 end
