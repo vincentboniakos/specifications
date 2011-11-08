b0fd90f2aa5ac25b834e4c997e7442489d23282c
Vincent Boniakos <vincent.boniakos@gmail.com>
2011-11-08 19:14:59 +0100

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Specifications::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+    :domain         => 'specs.5emeg.com'
   }
   config.action_mailer.delivery_method = :smtp
 
